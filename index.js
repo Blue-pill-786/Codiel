4a5f07ba1e8509a2f597e9fa415c1392501c9392
Blue-pill-786 <113605539+Blue-pill-786@users.noreply.github.com>
2023-05-14 01:05:44 +0530

    setting up static files

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
 const port =8000;
+const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
+//use express layouts
+app.use(expressLayouts);
 
+//extract styles and scripts from subpages into the layouts
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 //use express router
 app.use('/', require('./routes'));
+
+//set up our view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 
 app.listen(port, function(err){
